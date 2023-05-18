a38baadc86ff0852361f132a88253c61f17c7f50
gaonsoftdev <gaonsoftdev@gaonsoft.com>
2023-05-18 09:03:18 +0900

    메뉴추가

--- js/common.js
+++ js/common.js
@@ -54,8 +54,8 @@ GX._DATAS_ = {
 	},
 	pageUrls:{
 		'1':{link:'packProcessing.html', icon:'img/ic_mainmenu_01.png', groupTitle:'제품추적관리', subTitle:'제품출고처리'},//명세입고처리_TDI
-		'2':{link:'packProcessing.html', icon:'img/ic_mainmenu_02.png', groupTitle:'제품추적관리', subTitle:'구매납품처리'}, //재고실사등록_TDI
-		'3':{link:'packProcessing.html', icon:'img/ic_mainmenu_03.png', groupTitle:'제품추적관리', subTitle:'이동입력처리'}, 
+		'2':{link:'packProcessing2.html', icon:'img/ic_mainmenu_02.png', groupTitle:'제품추적관리', subTitle:'구매납품처리'}, //재고실사등록_TDI
+		'3':{link:'packProcessing3.html', icon:'img/ic_mainmenu_03.png', groupTitle:'제품추적관리', subTitle:'이동입력처리'}, 
 		'4':{link:'packProcessing4.html', icon:'img/ic_mainmenu_04.png', groupTitle:'제품추적관리', subTitle:'창고재고실사처리'}, 
 		// '5':{link:'otherShipmentProcessing.html', icon:'img/ic_mainmenu_05.png', groupTitle:'제품추적관리', subTitle:'기타출고처리'},
 		// '6':{link:'reworkProcessing.html', icon:'img/ic_mainmenu_06.png', groupTitle:'제품추적관리', subTitle:'재작업'}, 
